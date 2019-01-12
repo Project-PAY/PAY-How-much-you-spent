b5b238723d3434088c3f8a38ab2f4766f1084752
정윤재 <pewww@jeong-yunjaeui-MacBook-Pro.local>
2019-01-12 23:53:24 +0900

    :pencil: tslint 구문 수정 및 image import를 위한 image.d.ts 파일 내용 수정

--- pay/images.d.ts
+++ pay/images.d.ts
@@ -1,5 +1,8 @@
 declare module '*.svg'
-declare module '*.png'
+declare module '*.png' {
+    const value: any;
+    export = value;
+}
 declare module '*.jpg'
 declare module '*.jpeg'
 declare module '*.gif'
--- pay/src/components/Welcome/Welcome.tsx
+++ pay/src/components/Welcome/Welcome.tsx
@@ -5,7 +5,10 @@ const Welcome:React.ComponentType = () => (
   <div className="welcome">
     <h1>PAY</h1>
     <h2>How much you spent</h2>
-    <img />
+    <img
+      src="../../assets/images/img-setting-background.png"
+      alt="배경사진"
+    />
   </div>
 );
 
